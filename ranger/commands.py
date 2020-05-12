60d320c0ae10a556d7509784706b18b7c7d4436e
Joey Lee <skkshu@gmail.com>
2020-05-12 20:52:01 +0800

    fzf_select

--- ranger/commands.py
+++ ranger/commands.py
@@ -7,13 +7,14 @@
 # A simple command for demonstration purposes follows.
 # -----------------------------------------------------------------------------
 
+# The following must be at the beginning of the file
 from __future__ import (absolute_import, division, print_function)
-
-# You can import any python module as needed.
-import os
 
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
+
+import os
+from ranger.core.loader import CommandLoader
 
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
@@ -61,13 +62,13 @@ class my_edit(Command):
         # content of the current directory.
         return self._tab_directory_content()
 
+
 class mkcd(Command):
     """
     :mkcd <dirname>
 
     Creates a directory with the name <dirname> and enters it.
     """
-
     def execute(self):
         from os.path import join, expanduser, lexists
         from os import makedirs
@@ -84,7 +85,8 @@ class mkcd(Command):
 
             for m in re.finditer('[^/]+', dirname):
                 s = m.group(0)
-                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
+                if s == '..' or (s.startswith('.')
+                                 and not self.fm.settings['show_hidden']):
                     self.fm.cd(s)
                 else:
                     ## We force ranger to load content before calling `scout`.
@@ -92,69 +94,105 @@ class mkcd(Command):
                     self.fm.execute_console('scout -ae ^{}$'.format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
-# import os
-# from ranger.core.loader import CommandLoader
 
-# class extracthere(Command):
-    # def execute(self):
-        # """ Extract copied files to current directory """
-        # copied_files = tuple(self.fm.copy_buffer)
 
-        # if not copied_files:
-            # return
+class extracthere(Command):
+    def execute(self):
+        """ Extract copied files to current directory """
+        copied_files = tuple(self.fm.copy_buffer)
 
-        # def refresh(_):
-            # cwd = self.fm.get_directory(original_path)
-            # cwd.load_content()
+        if not copied_files:
+            return
 
-        # one_file = copied_files[0]
-        # cwd = self.fm.thisdir
-        # original_path = cwd.path
-        # au_flags = ['-X', cwd.path]
-        # au_flags += self.line.split()[1:]
-        # au_flags += ['-e']
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
 
-        # self.fm.copy_buffer.clear()
-        # self.fm.cut_buffer = False
-        # if len(copied_files) == 1:
-            # descr = "extracting: " + os.path.basename(one_file.path)
-        # else:
-            # descr = "extracting files from: " + os.path.basename(one_file.dirname)
-        # obj = CommandLoader(args=['aunpack'] + au_flags \
-                # + [f.path for f in copied_files], descr=descr, read=True)
+        one_file = copied_files[0]
+        cwd = self.fm.thisdir
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
 
-        # obj.signal_bind('after', refresh)
-        # self.fm.loader.add(obj)
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(copied_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in copied_files], descr=descr, read=True)
 
-# import os
-# from ranger.core.loader import CommandLoader
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
 
-# class compress(Command):
-    # def execute(self):
-        # """ Compress marked files to current directory """
-        # cwd = self.fm.thisdir
-        # marked_files = cwd.get_selection()
 
-        # if not marked_files:
-            # return
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
 
-        # def refresh(_):
-            # cwd = self.fm.get_directory(original_path)
-            # cwd.load_content()
+        if not marked_files:
+            return
 
-        # original_path = cwd.path
-        # parts = self.line.split()
-        # au_flags = parts[1:]
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
 
-        # descr = "compressing files in: " + os.path.basename(parts[1])
-        # obj = CommandLoader(args=['apack'] + au_flags + \
-                # [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
 
-        # obj.signal_bind('after', refresh)
-        # self.fm.loader.add(obj)
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
 
-    # def tab(self, tabnum):
-        # """ Complete with current folder name """
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
 
-        # extension = ['.zip', '.tar.gz', '.rar', '.7z']
-        # return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
+    def tab(self, tabnum):
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return [
+            'compress ' + os.path.basename(self.fm.thisdir.path) + ext
+            for ext in extension
+        ]
+
+
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+
+        else:
+            # match files and directories
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+
+        fzf = self.fm.execute_command(command,
+                                      universal_newlines=True,
+                                      stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
